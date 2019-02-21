acb70ac7339d0f5b11a1de3e82c34d5d8ec6e28e
TKMCode <tyler.code.maples@gmail.com>
2019-02-20 21:30:27 -0800

    adding file layout

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', 'Yes');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', 'No');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Burger', 'Yes');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double', 'No');
