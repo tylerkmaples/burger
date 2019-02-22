6e41a4a3ed015280a165531c796917d7f0d35d68
TKMCode <tyler.code.maples@gmail.com>
2019-02-21 17:37:24 -0800

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql")
+var connection; 
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db'
+    });
+};
 
 // CONNECT TO THE MYSQL SERVER AND SQL DATABASE
 connection.connect(function (err) {
