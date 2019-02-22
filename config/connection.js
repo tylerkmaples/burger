294dd113db6202c9513932603fff9e2669c52768
TKMCode <tyler.code.maples@gmail.com>
2019-02-21 20:24:35 -0800

    fixed connection config

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,15 @@
 var mysql = require("mysql")
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
     connection = mysql.createConnection({
-        host: 'm7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        user: 'bfal0b54trmm5532',
-        password: 'wz8f8scugy3fxv60',
-        database: 'yv3jcsu96xu11cjh',
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db',
         port: 3306
     });
 };
