ca527cb4fc493f6b62871a8e2b0c8152c45c2d82
TKMCode <tyler.code.maples@gmail.com>
2019-02-24 13:55:26 -0800

    anotha one

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 // Serve static content for the app from the "public" directory
 // the application directory
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
 
 // Parse application body as JSON
